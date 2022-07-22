5449aac2e6730549cb88141f916471375e9e7e33
reiya0104 <reiya0104@ezweb.ne.jp>
2022-07-22 19:59:54 +0900

    refactor: 正規表現を設定

--- src/commands/show.rs
+++ src/commands/show.rs
@@ -2,7 +2,6 @@ use crate::system::git_log::git_log;
 
 pub(crate) fn show(_args: Vec<String>) {
     let pretty = "format:\"[%ad] %h %an : %s\"";
-    let greps = vec!["start", "stop"];
-
-    git_log(pretty.to_string(), greps);
+    let grep = r#"^\(start\|stop\)\(:.\+\)\?$"#;
+    git_log(pretty, grep);
 }
--- src/system/git_log.rs
+++ src/system/git_log.rs
@@ -2,14 +2,11 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::process::{Command, Stdio};
 
-pub(crate) fn git_log(pretty: String, greps: Vec<&str>) {
+pub(crate) fn git_log(pretty: &str, grep: &str) {
     let pretty_opt = format!("--pretty={}", pretty);
-    let mut greps_opt: Vec<String> = greps.iter().map(|&x| format!("--grep={}", x)).collect();
+    let grep_opt = format!("--grep={}", grep);
 
-    let mut command_args: Vec<String> =
-        vec!["--no-pager".to_string(), "log".to_string(), pretty_opt];
-
-    command_args.append(&mut greps_opt);
+    let command_args: Vec<&str> = vec!["--no-pager", "log", &pretty_opt, &grep_opt];
 
     let mut child = Command::new("git")
         .args(command_args)
